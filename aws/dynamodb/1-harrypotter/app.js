fc8e173b379eeca00c5e794852da3c8bf87c715f
Harshil Tomar <harshiltomar20@gmail.com>
2024-05-29 11:37:57 +0530

    aws: express learn

--- aws/dynamodb/1-harrypotter/app.js
+++ aws/dynamodb/1-harrypotter/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const {
+  getCharacters,
+  getCharacterById,
+  addOrUpdateCharacter,
+  deleteCharacter,
+} = require("./dynamo");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -7,7 +15,55 @@ app.get("/", (req, res) => {
 
 app.get("/characters", async (req, res) => {
   try {
-    const characters = await getCharacter();
+    const characters = await getCharacters();
+    res.json(characters);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.get("/characters/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const characters = await getCharacterById(id);
+    res.json(characters);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.post("/characters", async (req, res) => {
+  const character = req.body;
+  try {
+    const newCharacter = await addOrUpdateCharacter(character);
+    res.json(newCharacter);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.put("/characters/:id", async (req, res) => {
+  const character = req.body;
+  const { id } = req.params;
+  character.id = id;
+  try {
+    const updatedCharacter = await addOrUpdateCharacter(character);
+    res.json(updatedCharacter);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+app.delete("/characters/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedCharacter = await deleteCharacter(id);
+    res.json(updatedCharacter);
   } catch (err) {
     console.error(err);
     res.status(500).json({ err: "Something went wrong" });
