131d6c27ba868a50ea8cf8c39531d748345d3f9e
Harshil Tomar <harshiltomar20@gmail.com>
2024-05-31 20:28:24 +0530

    aws: serverless complete

--- aws/serverless/hello.js
+++ aws/serverless/hello.js
@@ -7,7 +7,3 @@ module.exports.handler = async (event) => {
     body: JSON.stringify({ message: "Hello from Youtube Demo API" }),
   };
 };
-
-//on terminal do, export AWS Access Key and AWS Secret
-// DO `sls deploy` ( serverless deploy )
-// This created a .serverless folder which has all the files
