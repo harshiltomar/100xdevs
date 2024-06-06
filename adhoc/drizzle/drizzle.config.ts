684b6a0d0cdd71b86b1b546812fd6450fce76c46
Harshil Tomar <harshiltomar20@gmail.com>
2024-06-06 21:11:48 +0530

    learn: drizzle

--- adhoc/drizzle/drizzle.config.ts
+++ adhoc/drizzle/drizzle.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from "drizzle-kit";
 //@ts-ignore
 export default defineConfig({
   schema: "./src/drizzle/schema.ts",
+  out: "./src/drizzle/migrations",
+  //@ts-ignore
+  driver: "pg",
+  dbCredentials: {
+    //@ts-ignore
+    connectionString: process.env.DATABSE_URL as string,
+  },
+  verbose: true,
+  strict: true,
 });
