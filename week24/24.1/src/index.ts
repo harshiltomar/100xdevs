7e3432c4b3294ac3f490cdaaf4addac5e7ca5ea9
Harshil Tomar <harshil.tomar@xarterian.com>
2024-07-28 21:11:09 +0530

    init: express baisc

--- week24/24.1/src/index.ts
+++ week24/24.1/src/index.ts
@@ -1,7 +1,20 @@
-export function sum(a: number, b: number) {
-    return a+b;
-}
+// export function sum(a: number, b: number) {
+//     return a+b;
+// }
 
-export function multiply(a: number, b: number) {
-    return a*b;
-}
+// export function multiply(a: number, b: number) {
+//     return a*b;
+// }
+
+import express from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+app.post("/Sum", (req, res)=> {
+    const {a,b}= req.body;
+    const ans = a + b;
+
+    res.json(ans);
+})
