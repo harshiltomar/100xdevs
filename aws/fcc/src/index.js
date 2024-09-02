f246ed5e6b04ad428a8718ab2e976d52b3a0d23f
HarshilTomar20 <harshiltomar20@gmail.com>
2024-09-02 20:11:39 +0530

    chore: serverless

---
+++ aws/fcc/src/index.js
@@ -0,0 +1,30 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+    DATABASE_URL: process.env.DATABASE_URL,
+    DEBUG: process.env.DEBUG ==="1"
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// serverfull
+// app.listen(3000, ()=> {
+//   console.log("Listening at PORT:3000")
+// })
+
+module.exports.handler = serverless(app);
